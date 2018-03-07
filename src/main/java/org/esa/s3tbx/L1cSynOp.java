172bac1945fa9f8a34597be328a882a8fdc9c472
marpet <marco.peters@brockmann-consult.de>
2018-03-07 16:23:31 +0100

    very first operator

--- src/main/java/org/esa/s3tbx/L1cSynOp.java
+++ src/main/java/org/esa/s3tbx/L1cSynOp.java
@@ -1,9 +1,16 @@
 package org.esa.s3tbx;
 
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.gpf.GPF;
 import org.esa.snap.core.gpf.Operator;
 import org.esa.snap.core.gpf.OperatorException;
 import org.esa.snap.core.gpf.OperatorSpi;
 import org.esa.snap.core.gpf.annotations.OperatorMetadata;
+import org.esa.snap.core.gpf.annotations.SourceProduct;
+import org.esa.snap.core.gpf.annotations.TargetProduct;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @OperatorMetadata(alias = "L1C-SYN",
         label = "L1C-SYN Tool",
@@ -11,9 +18,46 @@ import org.esa.snap.core.gpf.annotations.OperatorMetadata;
         copyright = "Brockmann Consult GmbH",
         version = "0.1")
 public class L1cSynOp extends Operator {
+
+    @SourceProduct(alias = "olciProduct", label = "OLCI Product", description = "OLCI source product")
+    private Product olciSource;
+
+    @SourceProduct(alias = "slstrProduct", label = "SLSTR Product", description = "SLSTR source product")
+    private Product slstrSource;
+
+    @TargetProduct(label = "L1C SYN Product", description = "L1C SYNERGY output product")
+    private Product target;
+
     @Override
     public void initialize() throws OperatorException {
 
+        Product slstrInput = GPF.createProduct("Resample", getSlstrResampleParams(slstrSource), slstrSource);
+
+        HashMap<String, Product> sourceProductMap = new HashMap<>();
+        sourceProductMap.put("masterProduct", olciSource);
+        sourceProductMap.put("slaveProduct", slstrInput);
+        target = GPF.createProduct("Collocate", getCollocateParams(), sourceProductMap);
+
+    }
+
+    private Map<String, Object> getCollocateParams() {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("targetProductType", "S3_L1C_SYN");
+        params.put("renameMasterComponents", false);
+        params.put("renameSlaveComponents", false);
+        params.put("resamplingType", "NEAREST_NEIGHBOUR");
+        return params;
+    }
+
+    private HashMap<String, Object> getSlstrResampleParams(Product toResample) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("targetWidth", toResample.getSceneRasterWidth());
+        params.put("targetHeight", toResample.getSceneRasterHeight());
+        params.put("upsampling", "Nearest");
+        params.put("downsampling", "First");
+        params.put("flagDownsampling", "First");
+        params.put("resampleOnPyramidLevels", false);
+        return params;
     }
 
     public static class Spi extends OperatorSpi {
