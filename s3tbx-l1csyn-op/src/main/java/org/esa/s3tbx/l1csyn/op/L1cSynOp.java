f715117a0d419e7b958a6641c64f58cd72224c36
marpet <marco.peters@brockmann-consult.de>
2019-01-28 11:38:19 +0100

    introduced new packages / changed module names

---
+++ s3tbx-l1csyn-op/src/main/java/org/esa/s3tbx/l1csyn/op/L1cSynOp.java
@@ -0,0 +1,123 @@
+package org.esa.s3tbx.l1csyn.op;
+
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.gpf.GPF;
+import org.esa.snap.core.gpf.Operator;
+import org.esa.snap.core.gpf.OperatorException;
+import org.esa.snap.core.gpf.OperatorSpi;
+import org.esa.snap.core.gpf.annotations.OperatorMetadata;
+import org.esa.snap.core.gpf.annotations.Parameter;
+import org.esa.snap.core.gpf.annotations.SourceProduct;
+import org.esa.snap.core.gpf.annotations.TargetProduct;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("unused")
+@OperatorMetadata(alias = "L1CSYN",
+        label = "L1C SYN Tool",
+        authors = "Marco Peters, Roman Shevchuk",
+        copyright = "Brockmann Consult GmbH",
+        version = "1.0")
+public class L1cSynOp extends Operator {
+
+    @SourceProduct(alias = "olciProduct", label = "OLCI Product", description = "OLCI source product")
+    private Product olciSource;
+
+    @SourceProduct(alias = "slstrProduct", label = "SLSTR Product", description = "SLSTR source product")
+    private Product slstrSource;
+
+    @TargetProduct(label = "L1C SYN Product", description = "L1C SYNERGY output product")
+    private Product l1cTarget;
+
+    @Parameter(label = "Allowed time difference", defaultValue = "10", unit = "h",
+            description = "Allowed time difference between SLSTR and OLCI products")
+    private long allowedTimeDiff;
+
+    /*@Parameter(description = "The list of bands in the target product.", alias = "sourceBands", itemAlias = "band", rasterDataNodeType = Band.class)
+    private String[] targetBandNames;
+    The interface for band selection and default selection will be enabled for version-2 */
+
+
+    @Override
+    public void initialize() throws OperatorException {
+
+        if (!isValidOlciProduct(olciSource)) {
+            throw new OperatorException("OLCI product is not valid");
+        }
+
+        if (!isValidSlstrProduct(slstrSource)) {
+            throw new OperatorException("SLSTR product is not valid");
+        }
+
+        /* Will be enabled/reworked along with the band selection in version-2
+        if (targetBandNames == null) {
+            throw new OperatorException("Zero bands for target product are chosen");
+        }*/
+
+        checkDate(slstrSource, olciSource);
+
+        Product slstrInput = GPF.createProduct("Resample", getSlstrResampleParams(slstrSource), slstrSource);
+        HashMap<String, Product> sourceProductMap = new HashMap<>();
+        sourceProductMap.put("masterProduct", olciSource);
+        sourceProductMap.put("slaveProduct", slstrInput);
+        Product collocatedTarget = GPF.createProduct("Collocate", getCollocateParams(), sourceProductMap);
+        l1cTarget = GPF.createProduct("Reproject", getReprojectParams(), collocatedTarget);
+    }
+
+    static Map<String, Object> getReprojectParams() {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("resampling", "Nearest");
+        params.put("orthorectify", false);
+        params.put("noDataValue", "NaN");
+        params.put("includeTiePointGrids", true);
+        params.put("addDeltaBands", false);
+        params.put("crs", "EPSG:4326");
+        return params;
+    }
+
+    static Map<String, Object> getCollocateParams() {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("targetProductType", "S3_L1C_SYN");
+        params.put("renameMasterComponents", false);
+        params.put("renameSlaveComponents", false);
+        params.put("resamplingType", "NEAREST_NEIGHBOUR");
+        return params;
+    }
+
+    protected static HashMap<String, Object> getSlstrResampleParams(Product toResample) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("targetWidth", toResample.getSceneRasterWidth());
+        params.put("targetHeight", toResample.getSceneRasterHeight());
+        params.put("upsampling", "Nearest");
+        params.put("downsampling", "First");
+        params.put("flagDownsampling", "First");
+        params.put("resampleOnPyramidLevels", false);
+        return params;
+    }
+
+    private void checkDate(Product slstrSource, Product olciSource) throws OperatorException {
+        long slstrTime = slstrSource.getStartTime().getAsDate().getTime();
+        long olciTime = olciSource.getEndTime().getAsDate().getTime();
+        long diff = slstrTime - olciTime;
+        long diffInHours = (diff) / (1000 * 60 * 60);
+        if (diffInHours > allowedTimeDiff) {
+            throw new OperatorException("The SLSTR and OLCI products differ more than" + String.format("%d", diffInHours) + ". Please check your input times");
+        }
+    }
+
+    private boolean isValidOlciProduct(Product product) {
+        return product.getProductType().contains("OL_1") || product.getName().contains("OLCI");
+    }
+
+    private boolean isValidSlstrProduct(Product product) {
+        return product.getProductType().contains("SL_1") || product.getName().contains("SLSTR");
+    }
+
+    public static class Spi extends OperatorSpi {
+        public Spi() {
+            super(L1cSynOp.class);
+        }
+    }
+
+}
---
+++ s3tbx-l1csyn-op/src/test/java/org/esa/s3tbx/l1csyn/op/L1cSynOpTest.java
@@ -0,0 +1,89 @@
+package org.esa.s3tbx.l1csyn.op;
+
+import org.esa.snap.core.dataio.ProductIO;
+import org.esa.snap.core.datamodel.Band;
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.gpf.Operator;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+public class L1cSynOpTest {
+
+
+    @Test
+    public void testL1cSynOpTest() throws IOException {
+        String slstrFilePath = L1cSynOpTest.class.getResource("SLSTRSub100.nc").getFile();
+        String olciFilePath = L1cSynOpTest.class.getResource("OLCISub100.nc").getFile();
+
+        Product slstrProduct = ProductIO.readProduct(slstrFilePath);
+        Product olciProduct = ProductIO.readProduct(olciFilePath);
+        Operator l1cSynOp = new L1cSynOp();
+        l1cSynOp.setSourceProduct("olciProduct", olciProduct);
+        l1cSynOp.setSourceProduct("slstrProduct", slstrProduct);
+        Product result = l1cSynOp.getTargetProduct();
+        int numBands = result.getNumBands();
+        String productType = result.getProductType();
+        Band oa03band = result.getBand("Oa03_radiance");
+        int height = result.getSceneRasterHeight();
+        int width = result.getSceneRasterWidth();
+
+        assertEquals(42, height);
+        assertEquals(60, width);
+        assertEquals(414, numBands);
+        assertEquals("S3_L1C_SYN", productType);
+        assertNotNull(oa03band);
+    }
+
+    @Test
+    public void testGetCollocateParams() {
+        Map<String, Object> map = L1cSynOp.getCollocateParams();
+        boolean renameMasterComponents = (boolean) map.get("renameMasterComponents");
+        boolean renameSlaveComponents = (boolean) map.get("renameSlaveComponents");
+        String resamplingType = (String) map.get("resamplingType");
+        String targetProductType = (String) map.get("targetProductType");
+        assertFalse(renameMasterComponents);
+        assertFalse(renameSlaveComponents);
+        assertEquals("NEAREST_NEIGHBOUR", resamplingType);
+        assertEquals("S3_L1C_SYN", targetProductType);
+    }
+
+    @Test
+    public void testGetReprojectParams() {
+        Map<String, Object> map = L1cSynOp.getReprojectParams();
+        String resampling = (String) map.get("resampling");
+        boolean orthorectify = (boolean) map.get("orthorectify");
+        String crs = (String) map.get("crs");
+        assertEquals("Nearest", resampling);
+        assertFalse(orthorectify);
+        assertEquals("EPSG:4326", crs);
+
+    }
+
+    @Test
+    public void testGetResampleParameters() throws IOException {
+        String slstrFilePath = L1cSynOpTest.class.getResource("SLSTRSub100.nc").getFile();
+        Product slstrProduct = ProductIO.readProduct(slstrFilePath);
+
+        Map<String, Object> map = L1cSynOp.getSlstrResampleParams(slstrProduct);
+        int width = (int) map.get("targetWidth");
+        int height = (int) map.get("targetHeight");
+        String upsampling = (String) map.get("upsampling");
+        String downsampling = (String) map.get("downsampling");
+        String flagDownsampling = (String) map.get("flagDownsampling");
+        boolean resampleOnPyramidLevels = (boolean) map.get("resampleOnPyramidLevels");
+        assertEquals(30, width);
+        assertEquals(24, height);
+        assertEquals("Nearest", upsampling);
+        assertEquals("First", downsampling);
+        assertEquals("First", flagDownsampling);
+        assertFalse(resampleOnPyramidLevels);
+
+    }
+
+}
